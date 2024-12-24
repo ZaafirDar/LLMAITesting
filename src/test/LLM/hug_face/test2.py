8e0cd7ea3e334a1dbf44ed411f96641dc5fd3d6a
Zaafir Dar <zaafirdar@Zaf-Macbook-Pro-M3.local>
2024-12-25 03:45:05 +0500

    test 2

--- src/test/LLM/hug_face/test2.py
+++ src/test/LLM/hug_face/test2.py
@@ -3,8 +3,8 @@ from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassifica
 custom_tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased")
 
-classifier = pipeline('sentiment-analysis')
+classifier = pipeline('sentiment-analysis', model=model, tokenizer=custom_tokenizer)
 
-result = classifier ('I want to learn how to do AI Modal Benhmarking')
+result = classifier ('I love to code in python with pytorch')
 
 print(result)
